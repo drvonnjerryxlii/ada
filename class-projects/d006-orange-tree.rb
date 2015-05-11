698a64e3559eecbc148dcf177cfd6ac5c114b034
Jeri <drvonnjerryxlii@gmail.com>
2015-05-11 16:23:38 -0700

    current progress before dinner

---
+++ class-projects/d006-orange-tree.rb
@@ -0,0 +1,82 @@
+class OrangeTree
+
+  attr_reader :age, :alive, :height, :orange_count
+
+  def initialize
+    @age = 0
+    @alive = true
+    @height = 0
+    @orange_count = 0
+  end
+
+  def grow_oranges
+    if @age >= 3
+      @orange_count = @age * 2
+    end
+  end
+
+  def grow_taller
+    if @age < 5
+      @height += 2
+    else
+      @height += 0.1
+    end
+  end
+
+  def grow_older
+    @age += 1
+  end
+
+  def one_year_passes
+    if @alive == true
+      if @age < 10
+        grow_older
+        grow_taller
+        grow_oranges
+
+      else
+        puts "This tree has lived ten long years and has now died."
+        @orange_count = 0
+        @age = "dead"
+        @height = 0
+        @alive = false
+      end
+
+    else
+      puts "This tree is dead."
+    end
+  end
+
+  def pick_orange
+    if @orange_count != 0
+      @orange_count -= 1
+      puts "That was a very tasty orange!"
+    else
+      puts "There are no more oranges to pick this year."
+    end
+  end
+end
+
+
+class OrangeGrove
+  attr_reader :total_orange_count, :trees
+
+  def initialize
+    @trees = []
+  end
+
+  def plant_trees(number)
+    number.times do
+      @trees.push(OrangeTree.new)
+    end
+  end
+
+  def one_year_passes_grove
+    @total_orange_count = 0
+    @trees.each do |tree|
+      tree.one_year_passes
+      @total_orange_count += tree.orange_count
+    end
+  end
+
+end
