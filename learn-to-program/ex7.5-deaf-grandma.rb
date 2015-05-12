00427b52261eda332b9b985210a1a4a9034236ff
Jeri <drvonnjerryxlii@gmail.com>
2015-05-12 12:03:16 -0700

    updated trigger only 3 byes in a row

--- learn-to-program/ex7.5-deaf-grandma.rb
+++ learn-to-program/ex7.5-deaf-grandma.rb
@@ -1,10 +1,21 @@
-def deaf_grandma(user_input, bye_count)
+def deaf_grandma(user_input, bye_count, was_last_thing_bye=false)
+  unless was_last_thing_bye # if last thing WASN'T bye
+    bye_count = 0
+  end
+
   if user_input == "BYE"
+    was_last_thing_bye = true
     bye_count += 1
+
+    # finally,  grandma will stop shouting at you!
     if bye_count == 3
       puts "BYE, SONNY!"
       return
     end
+
+  else
+    was_last_thing_bye = false
+
   end
 
   if user_input == user_input.upcase
@@ -13,7 +24,7 @@ def deaf_grandma(user_input, bye_count)
     puts "HUH?!  SPEAK UP, SONNY!"
   end
 
-  return deaf_grandma($stdin.gets.chomp, bye_count)
+  return deaf_grandma($stdin.gets.chomp, bye_count, was_last_thing_bye)
 end
 
 puts "Say something to Grandma:"
