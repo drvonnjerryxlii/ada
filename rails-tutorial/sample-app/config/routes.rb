03f1a7f8c8768997eac7aaa8c235c16b0d5a2097
Jeri <drvonnjerryxlii@gmail.com>
2015-07-11 10:32:44 -0700

    disabled layout & got tests for title to run green

--- rails-tutorial/sample-app/config/routes.rb
+++ rails-tutorial/sample-app/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  resources :microposts
-  resources :users
-  # root 'application#hello'
-  root 'users#index'
+  get 'static_pages/about'
 end
