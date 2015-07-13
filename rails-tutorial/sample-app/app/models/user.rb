22f7f0faa0dc8e6a43986a3533227d4db316220d
Jeri <drvonnjerryxlii@gmail.com>
2015-07-13 09:37:53 -0700

    updating progress on ch-8 and switching back to master branch

--- rails-tutorial/sample-app/app/models/user.rb
+++ rails-tutorial/sample-app/app/models/user.rb
@@ -1,19 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
-
-  before_save { self.email.downcase! }
-
-  validates :name, presence: true, length: { maximum: 255 }
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-
-  validates :password, presence: true, length: { minimum: 6 }
-
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -27,19 +21,9 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  # Remembers a user in the database for use in persistent sessions.
   def remember
-    self.remember_token = ...
-    update_attribute(:remember_digest, ...)
-  end
-
-  # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-  # Forgets a user.
-  def forget
-    update_attribute(:remember_digest, nil)
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
